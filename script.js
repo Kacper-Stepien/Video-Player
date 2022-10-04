5c278a9ef559140e285b32d6360d1a06389073f2
Kacper Stępień <kacper2007x48@gmail.com>
2022-10-04 22:21:59 +0200

    add full screen functionality, add change sound functionality, add hidden menu which shows when mouseover event happen

--- script.js
+++ script.js
@@ -1,11 +1,22 @@
 const videoSelector = document.getElementById('video-selector');
+const videoContainer = document.getElementById('container')
 const video = document.getElementById('video');
+const menu = document.getElementById('menu');
 const playBtn = document.getElementById('play-btn');
 const stopBtn = document.getElementById('stop-btn');
 const timeline = document.getElementById('timeline');
 const timestamp = document.getElementById('timestamp');
+const volumeBtn = document.getElementById('volume-btn');
+const volumeBar = document.getElementById('volume');
+const fullScreenBtn = document.getElementById('full-screen-btn');
+
+let loadedVideo = false;
+let soundLevel = 0.5;
 
 function toggleVideoStatus() {
+    if (!loadedVideo)
+        return
+
     if (video.paused) {
         video.play();
     }
@@ -17,6 +28,9 @@ function toggleVideoStatus() {
 }
 
 function stopAndRestartVideo() {
+    if (!loadedVideo)
+        return
+
     video.currentTime = 0;
     video.play();
     updatePlayIcon();
@@ -53,16 +67,100 @@ function setVideoProgress() {
     video.currentTime = (+timeline.value * video.duration) / 100;
 }
 
+function loadNewVideo() {
+    video.pause();
+    setVideoSrcAttribute();
+    timeline.value = 0;
+    loadedVideo = true;
+}
+
 function setVideoSrcAttribute() {
     let movie = videoSelector.files[0];
     let file = movie;
     let url = URL.createObjectURL(file);
     video.src = url;
-    timeline.value = 0;
+}
+
+function changeVolume() {
+    video.volume = volumeBar.value;
+    if (video.volume !== 0) {
+        soundLevel = video.volume;
+        volumeBtn.innerHTML = '<i class="fa-solid fa-volume-high"></i>';
+    }
+    else {
+        volumeBtn.innerHTML = '<i class="fa-solid fa-volume-xmark"></i>';
+    }
+}
+
+function toggleFullScreen() {
+    const fullscreenElement =
+        document.fullscreenElement ||
+        document.mozFullScreenElement ||
+        document.webkitFullscreenElement ||
+        document.msFullscreenElement;
+    if (fullscreenElement) {
+        exitFullscreen();
+    } else {
+        launchIntoFullscreen(videoContainer);
+    }
+}
+
+function launchIntoFullscreen(element) {
+    if (element.requestFullscreen) {
+        element.requestFullscreen();
+    } else if (element.mozRequestFullScreen) {
+        element.mozRequestFullScreen();
+    } else if (element.webkitRequestFullscreen) {
+        element.webkitRequestFullscreen();
+    } else if (element.msRequestFullscreen) {
+        element.msRequestFullscreen();
+    } else {
+        element.classList.toggle('fullscreen');
+    }
+    video.classList.add('screen-full-size');
+    menu.classList.add('menu-full-screen');
+    fullScreenBtn.innerHTML = '<i class="fa-solid fa-compress"></i>';
+}
+
+function exitFullscreen() {
+    if (document.exitFullscreen) {
+        document.exitFullscreen();
+    } else if (document.mozCancelFullScreen) {
+        document.mozCancelFullScreen();
+    } else if (document.webkitExitFullscreen) {
+        document.webkitExitFullscreen();
+    }
+    video.classList.remove('screen-full-size');
+    menu.classList.remove('menu-full-screen');
+    fullScreenBtn.innerHTML = '<i class="fa-solid fa-expand"></i>'
+}
+
+function showMenu() {
+    menu.classList.remove('hidden');
+    setTimeout(() => {
+        menu.classList.add('hidden')
+    }, 4000);
+}
+
+function toggleSound() {
+    if (video.volume === 0) {
+        video.volume = soundLevel;
+        volumeBar.value = soundLevel;
+        volumeBtn.innerHTML = '<i class="fa-solid fa-volume-high"></i>';
+    }
+    else {
+        video.volume = 0;
+        volumeBar.value = 0;
+        volumeBtn.innerHTML = '<i class="fa-solid fa-volume-xmark"></i>';
+    }
 }
 
 // Event Listeners
-videoSelector.addEventListener('change', setVideoSrcAttribute);
+videoSelector.addEventListener('click', () => {
+    video.pause();
+    updatePlayIcon();
+});
+videoSelector.addEventListener('change', loadNewVideo);
 
 video.addEventListener('click', toggleVideoStatus);
 video.addEventListener('timeupdate', () => {
@@ -73,3 +171,9 @@ video.addEventListener('timeupdate', () => {
 playBtn.addEventListener('click', toggleVideoStatus);
 stopBtn.addEventListener('click', stopAndRestartVideo);
 timeline.addEventListener('change', setVideoProgress);
+
+volumeBtn.addEventListener('click', toggleSound);
+volumeBar.addEventListener('change', changeVolume);
+fullScreenBtn.addEventListener('click', toggleFullScreen);
+
+video.addEventListener('mouseover', showMenu);
